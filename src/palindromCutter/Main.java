4baa5cb57e5633600f7b7380044bb4a608ecb5b8
flaciGit <file.laszlo.n@gmail.com>
2019-02-02 18:05:24 +0100

    Finalizing

--- src/palindromCutter/Main.java
+++ src/palindromCutter/Main.java
@@ -21,39 +21,36 @@ public class Main {
 			l--;
 		}
 		return true;
-		
 	}
 	
-	
-	public static void main(String[] args) {
-		
+	public static void printAllPalindromes() {
 		
 		int start = 0;
 		int end = palindrom.length()-1;
-		
 		
 		while(start < end) {
 			
 			int i = end;
 			
-			
 			while(i > start) {
-				//System.out.println("start: "+start+ " end: "+end+ " i: "+i);
 				
 				if(isPalindrom(start,i))
 					collectPalindroms.add(palindrom.substring(start,i+1));
 				i--;
 			}
 			start++;
-			
 		}
-		
 		
 		for(int i = 0; i < collectPalindroms.size();i++) {
-			System.out.println(collectPalindroms.get(i));
+			System.out.print(collectPalindroms.get(i) + " ");
 			
 		}
-		
 	}
-
+	
+	public static void main(String[] args) {
+		
+		System.out.println("String: "+palindrom);
+		System.out.print("All palindromes: (minimum length: 2 characters) ");
+		printAllPalindromes();
+	}
 }
